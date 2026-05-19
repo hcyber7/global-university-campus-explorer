5d496b173297f1181fed7b8aed84e103d532e577
Ashutosh Ballan <49350004+ashuballan11@users.noreply.github.com>
2026-05-19 22:07:32 +0200

    API for Universities

--- api/university_api.py
+++ api/university_api.py
@@ -1,1 +1,19 @@
+import requests
 
+BASE_URL = "http://universities.hipolabs.com/search"
+
+
+def search_universities(country):
+
+    url = f"{BASE_URL}?country={country}"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()
+
+    return []
+
+
+results = search_universities("Germany")
+print(results)
