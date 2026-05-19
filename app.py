546680d0dfa7ea4b2a47a8253b12a2e0cfaf4b30
Hamza <hamzaguzelel2007@gmail.com>
2026-05-19 20:38:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 print("Global University & Campus Explorer")
+
+
+import requests
+
+BASE_URL = "http://universities.hipolabs.com/search"
+
+
+def search_universities(country):
+
+    url = f"{BASE_URL}?country={country}"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()
+
+    return
