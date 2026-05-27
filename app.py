92d2c636a3ddc478fc245783b0ff9c11f2efa942
Hamza <hamzaguzelel2007@gmail.com>
2026-05-28 01:51:21 +0400

    Added university search functionalities, dynamic results display, and validation

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from API.University_api import search_universities
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+
+    if request.method == "POST":
+
+        country = request.form["country"]
+
+        universities = search_universities(country)
+
+        return render_template(
+            "index.html",
+            universities=universities,
+            searched_country=country
+        )
+    
     return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
-
