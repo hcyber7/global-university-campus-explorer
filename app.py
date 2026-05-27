489bba2dda57c96487adc3bdef6c95848744e774
Hamza <hamzaguzelel2007@gmail.com>
2026-05-28 02:37:35 +0400

    Added favorites system and Database Integration

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from API.University_api import search_universities
+from Database.database import add_to_favorites, get_favorites
 
 app = Flask(__name__)
 
@@ -19,6 +20,27 @@ def home():
         )
     
     return render_template("index.html")
+
+@app.route("/favorite", methods=["POST"])
+def favorite():
+
+    name = request.form["name"]
+    country = request.form["country"]
+    website = request.form["website"]
+
+    add_to_favorites(name, country, website)
+
+    return f"{name} added to favorites!"
+
+@app.route("/favorites")
+def favorites():
+
+    favorites_list = get_favorites()
+
+    return render_template(
+        "favorites.html",
+        favorites=favorites_list
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
